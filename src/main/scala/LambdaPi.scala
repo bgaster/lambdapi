924157426c454322d253ce1ebc1e1dda9958a7a7
Benedict R. Gaster <benedict.gaster@uwe.ac.uk>
2015-02-25 21:24:23 +0000

    Basic implementation complete. Need to tidy up the pretty printer and add loading file support.

--- src/main/scala/LambdaPi.scala
+++ src/main/scala/LambdaPi.scala
@@ -7,14 +7,14 @@
  * 
  *  The orginal paper describes a Haskell implementaiton and at the
  *  time of writing I was learning the basics of implementing
- *  dependetly type languages and at the same time introducing myself
- *  to Scala, hence it seemed interesing to reimplement their work in Scala.
+ *  dependetly type languages and also introducing myself to Scala,
+ *  hence it seemed interesing to reimplement their work in Scala.
  * 
  */
 package org
 
 package object utils {
-  object Parser extends org.kiama.output.PrettyPrinter {
+  object Printer extends org.kiama.output.PrettyPrinter {
     def parensIf( b : Boolean , d : Doc ) : Doc = if (b) parens(d) else d
   }
 }
@@ -77,16 +77,23 @@ package object lambdapi {
   def vfree ( n : Name ) : Value =
     VNeutral (NFree (n))
 
-  type Env = Vector[Value]
+    def lookup[A] ( k : Name, c : Vector[(Name,A)]) : A =
+      c match {
+        case IndexedSeq() => throw ErrorException.create("undefined identifier")
+        case a +: cs      => if (a._1 == k) a._2 else lookup(k,cs)
+      }
+
+  // now we define the evaluator
+  type Env = (Vector[(Name,Value)], Vector[Value])
 
   object Eval {
     def eval ( t : TermInfer, env : Env ) : Value =
       t match  {
         case Ann(e, t)  => eval(e, env)
         case Star()     => VStar()
-        case Pi(t1, t2) => VPi(eval(t1, env), (x) => eval(t2, x +: env))
-        case Free(x)    => vfree(x)
-        case Bound(i)   => env(i)
+        case Pi(t1, t2) => VPi(eval(t1, env), (x) => eval(t2, (env._1, x +: env._2)))
+        case Free(x)    => (Try(lookup(x,env._1)) recover { case _ => vfree(x) }) get
+        case Bound(i)   => env._2(i)
         case App(f,e)   => vapp( eval(f, env), eval(e, env) )
       }
 
@@ -100,7 +107,7 @@ package object lambdapi {
     def eval ( t : TermCheck, env : Env ) : Value =
     t match {
       case Inf(i) => eval(i, env)
-      case Lam(e) => VLam ((x) => eval( e, x +: env ))
+      case Lam(e) => VLam ((x) => eval( e, (env._1, x +: env._2 )))
     }
 
     def quote( v : Value ) : TermCheck = quote(0, v)
@@ -127,33 +134,22 @@ package object lambdapi {
       }
   }
 
+  // now define the typechecker
+
   type Result[A] = Try[A]
   type Type = Value
   type Context = Vector[(Name,Type)]
 
   object Typing {
 
-    def lookup ( k : Name, c : Context) : Result[Type] =
-      c match {
-        case IndexedSeq() => throw ErrorException.create("undefined identifier")
-        case a +: cs      => if (a._1 == k) Success(a._2) else lookup(k,cs)
-      }
-
     def typeTerm ( c : Context, t : TermInfer ) : Result[Type] =
       typeTerm(0, c, t)
-
-    def dummy ( s : String ) : Result[Type] = {
-      println(s)
-      Success(VStar())
-    }
 
     def typeTerm ( i : Int, c : Context, t : TermInfer ) : Result[Type] =
       t match {
         case Ann(e, p)     => for {
-          _ <- dummy("before")
           _ <- typeTerm(i, c, p, VStar())
-          _ <- dummy("after")
-          val t = Eval.eval(p, Vector())  
+          val t = Eval.eval(p, (Vector(), Vector()))  
           tt <- typeTerm(i, c, e, t)
         } yield(t)
 
@@ -161,25 +157,21 @@ package object lambdapi {
 
         case Pi(p1, p2)    => for {
           _ <- typeTerm(i, c, p1, VStar())
-          val t = Eval.eval(p1, Vector())
+          val t = Eval.eval(p1, (Vector(), Vector()))
           _ <- typeTerm(i+1, (Local(i),t) +: c, subst(0,Free(Local(i)), p2), VStar())
         } yield(VStar())
 
         case Bound(i)     =>
           throw ErrorException.create("typeTerm should not have received bound")
 
-        case Free(n)      =>
-          lookup(n,c) match {
-            case Success(ty) => Success(ty)
-            case _          => throw ErrorException.create("undefined identifier")
-          }
+        case Free(n)      => Try( lookup(n,c) )
 
         case App(e1,e2) => for {
           s <- typeTerm(i, c, e1)
           r <- s match {
             case VPi(t1, t2) => for {
               tt <- typeTerm(i, c, e2, t1)
-            } yield( t2( Eval.eval(e2,Vector()) ) )
+            } yield( t2( Eval.eval(e2,(Vector(), Vector())) ) )
             case _ =>
               throw ErrorException.create("illegal application")
           }
@@ -221,15 +213,17 @@ package object lambdapi {
       }
   }
 
+  // parser for LambdaPi
+
   trait Parser extends PositionedParserUtilities {
     import scala.collection.immutable.Seq
     import scala.language.postfixOps
 
-    class Bindings( e: List[String] )
+    class Bindings( b : Boolean, e: List[String] )
         extends PackratParser[(List[String], List[TermCheck])] {
 
       // parse one or more parens bindings
-      class BindingsRec( e: List[String], ts: List[TermCheck] )
+      class BindingsRec( b : Boolean, e: List[String], ts: List[TermCheck] )
         extends PackratParser[(List[String], List[TermCheck])] {
 
         lazy val pp : PackratParser[(String,TermCheck)] = parens (
@@ -237,11 +231,17 @@ package object lambdapi {
             { case v ~ ":" ~ e => (v,e) })
 
         def apply(s: org.lambdapi.Parser.Input) = pp(s) match {
-          case Success(a, rem) =>
-            (new BindingsRec(a._1 :: e, a._2 :: ts))(rem) match {
-              case Success(aa, remm) => Success(aa,remm)
-              case _                 => Success((a._1 :: e, a._2 :: ts), rem)
-            }
+          case Success(a, rem) => {
+            val bi = if (b) new BindingsRec(b, a._1 :: e, a._2 :: ts)
+                     else   new BindingsRec(b, e, ts)
+
+              bi(rem) match {
+                case Success(aa, remm) =>
+                  if (b) Success(aa,remm)
+                  else Success((a._1 :: (aa._1 ++ e), a._2 :: (aa._2 ++ ts)), remm)
+                case _                 => Success((a._1 :: e, a._2 :: ts), rem)
+              }
+          }
           case f: Failure => f
           case _ => throw new RuntimeException // remove warnings
         }
@@ -253,7 +253,7 @@ package object lambdapi {
       { case v ~ ":" ~ t => (v :: e, List(t)) }
 
       def apply(s: org.lambdapi.Parser.Input) =
-        new BindingsRec(e, List())(s) match {
+        new BindingsRec(b, e, List())(s) match {
           case Success(a, rem) => Success(a,rem)
           case f: Failure => {
             alt(s)
@@ -262,18 +262,18 @@ package object lambdapi {
       }
     }
 
-    def bindings(e : List[String]) :
-        PackratParser[(List[String], List[TermCheck])] = new Bindings(e)
+    def bindings(b : Boolean, e : List[String]) :
+        Parser[(List[String], List[TermCheck])] = new Bindings(b, e)
 
-    def pTermInfer ( e : List[String] ) : PackratParser[TermInfer] = 
+    def pTermInfer ( e : List[String] ) : Parser[TermInfer] = 
      presZero(e)
 
-    def presZero ( e : List[String] ) : PackratParser[TermInfer] =
+    def presZero ( e : List[String] ) : Parser[TermInfer] =
       forall(e) | funarrow(e) | presOne(e)
 
-    def forall ( e : List[String] ) : PackratParser[TermInfer] =
+    def forall ( e : List[String] ) : Parser[TermInfer] =
       Parser[TermInfer] { in =>
-        ("forall" ~  new Bindings(e))(in) match {
+        ("forall" ~  new Bindings(true, e))(in) match {
           case Success("forall" ~ bs, rem) => ("." ~ pTermCheck(bs._1) ^^
               { case "." ~ t =>
                 bs._2.tail.foldLeft(Pi(bs._2 head,t)) ((a,b) => Pi(bs._2 head, Inf(a)))
@@ -283,39 +283,42 @@ package object lambdapi {
         }
       }
 
-    def funarrow ( e : List[String] ) : PackratParser[TermInfer] = {
+    def funarrow ( e : List[String] ) : Parser[TermInfer] = {
       presOne(e) ~ "->" ~ pTermCheck("" :: e) ^^ {
         case ti ~ "->" ~ tc => Pi(Inf(ti), tc) }
     }
 
-    def presOne ( e : List[String] ) : PackratParser[TermInfer] =
+    def presOne ( e : List[String] ) : Parser[TermInfer] =
       annLambda(e) | presTwo(e)
 
-    def annLambda ( e : List[String] ) : PackratParser[TermInfer] =
-      (presTwo(e) ~ "::" ~ pTermCheck(e) ^^ {
-        case ti ~ "::" ~ tc => Ann(Inf(ti), tc)
-      }) | (parens(pLambda(e)) ~ "::" ~ pTermCheck(e) ^^ {
-        case tc1 ~ "::" ~ tc2 => Ann(tc1,tc2)
+    def annLambda ( e : List[String] ) : Parser[TermInfer] =
+      (presTwo(e) ~ ":" ~ pTermCheck(e) ^^ {
+        case ti ~ ":" ~ tc => Ann(Inf(ti), tc)
+      }) | (parens(pLambda(e)) ~ ":" ~ pTermCheck(e) ^^ {
+        case tc1 ~ ":" ~ tc2 => Ann(tc1,tc2)
       })
 
-    def presTwo ( e : List[String] ) : PackratParser[TermInfer] =
-      presThree(e) ~ rep(pTermCheck(e)) ^^ {
+    def presTwo ( e : List[String] ) : Parser[TermInfer] =
+      presThree(e) ~ rep(pTermCheckPresThree(e)) ^^ {
         case t ~ ts => ts.foldLeft(t) ((a,b) => a $$ b) } | presThree(e)
 
-    def presThree ( e : List[String] ) : PackratParser[TermInfer] =
-      parens( pTermInfer(e) ) | star | identifier(e)
+    def presThree ( e : List[String] ) : Parser[TermInfer] =
+       star | identifier(e) | parens( pTermInfer(e) )
 
-    def identifier ( e : List[String] ) : PackratParser[TermInfer] =
+    def identifier ( e : List[String] ) : Parser[TermInfer] =
       variable ^^ { v => e.indexOf(v) match {
         case -1 => Free(Global (v))
         case n  => Bound(n)
       } }
 
-    def  pTermCheck ( e : List[String] ) : PackratParser[TermCheck] =
+    def  pTermCheck ( e : List[String] ) : Parser[TermCheck] =
       pLambda(e) | pTermInfer(e) ^^ { t => Inf(t) } | parens( pLambda(e) )
 
+    def  pTermCheckPresThree ( e : List[String] ) : Parser[TermCheck] =
+      pLambda(e) | presThree(e) ^^ { t => Inf(t) } | parens( pLambda(e) )
+
     // maybe I'm being dumb but it would be nice if Monad syntax worked here!!
-    def pLambda(e : List[String] ) : PackratParser[TermCheck] =
+    def pLambda(e : List[String] ) : Parser[TermCheck] =
       Parser[TermCheck] { in =>
         ("\\" ~ rep1(variable))(in) match {
           case Success("\\" ~ xs, rem) => ("." ~ pTermCheck(xs.reverse ++ e) ^^
@@ -327,21 +330,26 @@ package object lambdapi {
 
     lazy val star = "*" ^^ { _ => Star() }
 
-    lazy val variable : PackratParser[String] =
+    lazy val variable : Parser[String] =
       idn
 
-    lazy val idn : PackratParser[String] =
+    lazy val idn : Parser[String] =
       not (keyword) ~> "[a-zA-Z][a-zA-Z0-9]*".r
 
     lazy val keyword : Parser[String] =
       "let" | "assume" | "forall"
 
-    def parens[A]( p : PackratParser[A] ) : PackratParser[A] =
+    def parens[A]( p : Parser[A] ) : Parser[A] =
       "(" ~> p <~ ")"
 
   }
 
+  // We need this as Parser is only a trait and we want to be able to
+  // direclty reference members (i still find the Scala parser
+  // combinators somewhat odd for this reason)
   object Parser extends Parser
+
+  // Pretty printer for LambdaPi
 
   object Printer extends org.kiama.output.PrettyPrinter {
 
@@ -358,6 +366,21 @@ package object lambdapi {
     def pretty (e : TermInfer) : String =
       super.pretty (show(0,e)._2)
 
+    val vars : Stream[String] =
+      for {
+        n <- "" #:: (Stream.from(1).map { i => i.toString } )
+        c <- Stream('x','y','z') ++ Stream.range('a', 'x')
+      } yield ( c.toString ++ n )
+
+
+    implicit class Vars(val s : Stream[String]) extends AnyVal {
+      private def element(s : Stream[String], i : Int) : String =
+        if (i <= 0) s.head else element(s.tail, i-1)
+
+      def !!(i: Int) = element(s, i)
+    }
+
+
     def show (i : Int, e : TermInfer) : (Int, Doc) =
       e match {
         case Ann(e,t) => {
@@ -367,10 +390,10 @@ package object lambdapi {
         case Star() => (i, "*")
         case Pi(t1, t2) => {
           val (i1, d1) = show(i, t1)
-          val (i2, d2) = show(i1+1,t2)
-          (i2, ("forall" <+> parens(i1.toString <+> ":" <+> d1) <+> "." <+> parens( d2 ) ))
+          val (i2, d2) = show(i+1,t2)
+          (i2, ("forall" <+> parens((vars !! i) <+> ":" <+> d1) <+> "." <+> parens( d2 ) ))
         }
-        case Bound(ii) => (i, value("x" + ii.toString))
+        case Bound(ii) => (i, vars !! (i - ii - 1))
         case Free(n)  => (i, show(n))
         case App(lhs, rhs) =>
           (i, parens( show(i, lhs)._2 ) <+> parens( show(i, rhs)._2 ))
@@ -384,7 +407,7 @@ package object lambdapi {
         case Inf(t) => show(i, t)
         case Lam(l) => {
           val (ii,d) = show(i, l)
-          (ii+1, "\\x" <> ii.toString <> "." <+> d)
+          (ii+1, "\\" <> (vars !! ii) <> "." <+> d)
         }
       }
 
@@ -392,22 +415,7 @@ package object lambdapi {
       super.pretty (show (v))
 
     def show( v : Value ) : Doc = show(0, Eval.quote(v))._2
-
   }
-
-  val id = Lam ( Lam ( Inf( Bound( 0 ) )) )
-  val const = Lam (Lam (Inf (Bound (1))))
-  val tfree = (a : String)  => Free (Global(a))
-  val free  = (x : String ) => Inf (Free (Global(x)))
-
-  val term2 = Ann ( id, (Inf  (Pi  (Inf (Star()), (Inf (Pi  (Inf(Bound(0)),  (Inf (Bound (1))))))))))
-  val term3 = term2 $$ free("A")
-  val term4 = term3 $$ free("y")
-
- val env1 : Context = Vector(
-   (Global("y"), vfree(Global("A"))),
-   (Global("A"), VStar()) )
-
 } // package object lambdapi
 
 
@@ -416,15 +424,18 @@ package object commands {
   import org.kiama.util.ParserUtilities
   import org.lambdapi._
 
+  var env     : Vector[(Name,Value)] = Vector()
+  var context : Context = Vector()
+
   /*
    *  Following in the sprit of the orignal work we support a 
    *  simple, Gofer inspired, interpreter
    */
   sealed abstract class Command extends Attributable
-  case class CExpr( t : TermCheck )             extends Command
-  case class CLet( n : Name, t : TermCheck )    extends Command
-  case class CAssume( n : Name, t : TermCheck ) extends Command
-  case class CType( t : TermCheck )             extends Command
+  case class CExpr( t : TermInfer )             extends Command
+  case class CLet( n : Name, t : TermInfer )    extends Command
+  case class CAssume( as : List[(Name, TermCheck)] ) extends Command
+  case class CTypeOf( t : TermInfer )           extends Command
   case class CBrowse()                          extends Command
   case class CLoad ( filename : String )        extends Command
   case class CHelp ()                           extends Command
@@ -438,18 +449,34 @@ package object commands {
       phrase (command)
 
     lazy val command : PackratParser[Command] =
-       cexpr | internal
+       assume | let | cexpr |  internal
 
-    lazy val cexpr = pTermCheck(List()) ^^
+    lazy val cexpr = pTermInfer(List()) ^^
        { case t => CExpr(t) }
 
-    lazy val internal = ":" ~> (help | browse)
+    lazy val assume = ("assume" | "a") ~ bindings(false, List()) ^^ {
+      case a ~ bs => CAssume( (bs._1 zip bs._2) map (v => (Global(v._1), v._2)) )
+    }
+
+    lazy val let = ("let" | "l") ~ variable ~ "=" ~ pTermInfer(List()) ^^ {
+      case l ~ v ~ "=" ~ e => CLet(Global(v), e)
+    }
+
+    lazy val internal = ":" ~> (help | browse | typeOf | load)
+
+    lazy val load = ("load" | "l") ~> filepath ~ filename ^^ { case p ~ fn => CLoad(p + fn) }
+
+    lazy val filepath : Parser[String] = "[\\w/]*/".r
+    lazy val filename : Parser[String] = "[\\w]+\\.lpi".r
+
     lazy val help   = ("help" | "h" | "?")  ^^ { _ => CHelp() }
+    lazy val typeOf = ("type" | "t") ~ pTermInfer(List()) ^^ { case s ~ t => CTypeOf(t) } 
     lazy val browse = ("browse" | "b") ^^ { _ => CBrowse() }
   }
 
 } // package object commands
 
+// Finally we define the REPL based on Kiama's
 object Main extends org.kiama.util.ParsingREPL[org.commands.Command]
             with org.commands.CommandParser {
   import org.commands._
@@ -481,8 +508,52 @@ object Main extends org.kiama.util.ParsingREPL[org.commands.Command]
   override def process (c : Command, config : REPLConfig) {
     val output = config.output
     c match {
-      case CHelp() => displayHelp()
-      case CExpr(e) => println(e.toString)
+      case CHelp()    => displayHelp()
+
+      case CExpr(e)   => org.lambdapi.Typing.typeTerm(context, e) match {
+        case scala.util.Success(t)  =>
+          println(
+            org.lambdapi.Printer.pretty(org.lambdapi.Eval.eval(e, (env, Vector()))) +
+            " : " +
+            org.lambdapi.Printer.pretty(t)
+          )
+        case scala.util.Failure(ex) => println(s"ERROR: ${ex.getMessage}")
+        }
+
+      case CAssume(as) => 
+        as map ( a => {
+        val e = org.lambdapi.Ann( a._2, (org.lambdapi.Inf(org.lambdapi.Star())) )
+        org.lambdapi.Typing.typeTerm(context, e) match {
+          case scala.util.Success(t)  =>
+            context = (a._1, org.lambdapi.Eval.eval(a._2, (env, Vector()))) +: context
+          case scala.util.Failure(ex) => println(s"ERROR: ${ex.getMessage}")
+        }
+        })
+
+      case CLet(n, e) => org.lambdapi.Typing.typeTerm(context, e) match {
+        case scala.util.Success(t)  => {
+          env = (n, org.lambdapi.Eval.eval(e, (env, Vector()))) +: env
+          context = (n, t) +: context
+          println(
+            org.lambdapi.Printer.pretty(n) + " : " + org.lambdapi.Printer.pretty(t)
+          )
+        }
+        case scala.util.Failure(ex) => println(s"ERROR: ${ex.getMessage}")
+      }
+
+      case CBrowse() => {
+        context.foldRight( () ) ( (c,b) =>
+          println(org.lambdapi.Printer.pretty(c._1) + " : " + org.lambdapi.Printer.pretty(c._2)) )
+      }
+
+      case CLoad(fn) => println(fn)
+
+      case CTypeOf(e) => org.lambdapi.Typing.typeTerm(context, e) match {
+        case scala.util.Success(t)  => println(org.lambdapi.Printer.pretty(t))
+        case scala.util.Failure(ex) => println(s"ERROR: ${ex.getMessage}")
+      }
+        // still need to add the remaining commands...
+
       case _ => throw new RuntimeException // Should not happen
     }
   }
